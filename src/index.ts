495d945060b86eee7553950c96e4680860685422
newcodesubed <subedshahxi@gmail.com>
2026-03-25 17:07:12 +0545

    feat: add express for api

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
+import express from "express";
+import { getTodosWithStats } from "./services/todo.service";
 
-import { createDay, getDays } from "./repositories/day.repository";
-import { createTodo, getTodosByDay, updateTodo, deleteTodo, getTodosByDate, getTodoStatsByDate } from "./repositories/todo.repository";
+const app = express();
 
-const start = async () => {
-  // const newTodo = await createTodo('1', 'go to gym');
-  // console.log("Inserted", newTodo);
-  // const todos = await getTodosByDay(1);
-  // console.log("All todos", todos);
-  // const updatedTodo = await updateTodo(2, 'Buy milks', true);
-  // console.log("Updated", updatedTodo);
-  // const deletedTodo = await deleteTodo('1');
-  // console.log("Deleted", deletedTodo);
-  // const todos = await getTodosByDate("2026-03-25");
-  // console.log(todos);
-  const stats = await getTodoStatsByDate("2026-03-25");
-  console.log(stats);
+app.get("/todos/:date", async (req, res) => {
+  const data = await getTodosWithStats(req.params.date);
+  res.json(data);
+});
 
-};
-
-start();
+app.listen(3000, () => {
+  console.log("Server running");
+});
