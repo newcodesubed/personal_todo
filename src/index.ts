c28fcc67ec4306ab8ffb231d02b37f92f542dde8
newcodesubed <subedshahxi@gmail.com>
2026-03-26 13:44:16 +0545

    refactor: add asnc handler and global error handler

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.json());
 
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err);
+
+  res.status(500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
+
 app.use('/todos', todoRoutes);
 
 app.listen(3000, () => {
